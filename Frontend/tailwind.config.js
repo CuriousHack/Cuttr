4680c87cebef573280c83b6224bd3b5464c3f3a4
Akande Lateef <akandelateef0@gmail.com>
2024-12-04 10:55:13 +0100

    Add:Vue cli changed to vite

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,12 +1,18 @@
-// tailwind.config.js
-module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+      gridTemplateColumns: {
+        '70/30': '70% 28%',
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
