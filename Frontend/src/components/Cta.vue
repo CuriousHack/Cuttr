949ed8e1540dee3192c00ea1f516e89b15f4d98e
Akande Lateef <akandelateef0@gmail.com>
2024-12-05 01:37:26 +0100

    add: Cta button

---
+++ Frontend/src/components/Cta.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="boostSection w-full h-auto flex flex-col min-h-64 items-center justify-center gap-8">
+      <p class="md:text-5xl text-3xl font-bold text-white">Boost your links today</p>
+      <button class="text-xl font-semibold py-3 bg-[#29D1D1] text-white rounded-full px-6 cursor-pointer hover:bg-[#29d1d1bb]">Get Started</button>
+    </div>
+</template>
