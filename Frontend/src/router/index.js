6f63556cbe73a0c1be1c178354336a7e0bd41f34
Akande Lateef <akandelateef0@gmail.com>
2024-12-05 02:22:49 +0100

    update: key route

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -13,7 +13,7 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/:key([A-Za-z0-9+/=]+)',
+      path: '/:key',
       name: 'key',
       component: KeyView,
     },
