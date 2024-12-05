f01bcf9682638390f0600eee07021a50ffd3258f
Akande Lateef <akandelateef0@gmail.com>
2024-12-05 01:36:49 +0100

    fix:Hero Section

---
+++ Frontend/src/components/Hero.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="w-full flex items-center justify-center relative overflow-hidden">
+    <div class="w-full flex md:flex-row flex-col-reverse items-center justify-between">
+      <div
+      class="flex flex-col gap-8 md:items-start items-center md:text-left text-center md:w-1/2 w-11/12 md:pt-32 pt-6 md:pb-56 pb-40 md:pl-32 pl-0">
+      <div class="md:text-7xl text-4xl font-extrabold md:leading-[80px] leading-[50px] text-[#302F35]">More than
+        just shorter links</div>
+        <div class="text-xl text-[#BFBFBF]">Build your brand’s recognition and get detailed insights
+        on how your links are performing.
+      </div>
+      <button class="text-xl font-semibold py-3 bg-[#29D1D1] text-white rounded-full px-6 cursor-pointer hover:bg-[#29d1d1bb] ">Get Started</button>
+    </div>
+      <div class="md:w-1/2 w-full h-auto relative flex items-center justify-center">
+        <img src="../assets/images/illustration-working.svg" alt=""
+          class="md:absolute relative md:-right-28 -right-20 md:-bottom-40 bottom-0">
+      </div>
+    </div>
+    
+  </div>
+  
+</template>
